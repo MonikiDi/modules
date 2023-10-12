01c238eda0443af67a146b3adaa8d0a43ecfaef9
Denis Grigorev <d1e9n9i2s@gmail.com>
2023-10-12 20:18:57 +0300

    axios and working with Marvel api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Привет2");
+import 'regenerator-runtime/runtime';
+import {API_URL, URL_COMICS } from './constans/api';
+import { getDataApi } from './utils/getDataApi';
+
+(async () => {
+    const data = await getDataApi.getData(API_URL + URL_COMICS);
+    console.log(data);
+})();
