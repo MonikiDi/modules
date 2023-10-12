a5ea0428fb4854b9e0f12e673c7b3b556f977f39
Denis Grigorev <d1e9n9i2s@gmail.com>
2023-10-12 22:36:15 +0300

    event on item with receiving url for characters

--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -1,25 +1,26 @@
-import { API_URL, IMG_STANDART_XLARGE, URL_COMICS, IMG_NOT_AVALILABLE } from '../../constans/api.js';
+import { API_URL, IMG_STANDART_XLARGE, URL_COMICS, IMG_NOT_AVALILABLE, URL_CHARACTERS } from '../../constans/api.js';
 import { getDataApi } from '../../utils/getDataApi.js';
 import { ROOT_INDEX } from '../../constans/root.js';
 import './Comics.css';
+import App from '../App/App.js';
 
 class Comics {
 
     async render() {
         const data = await getDataApi.getData(API_URL + URL_COMICS);
-
         let htmlContent = '';
 
         data.forEach(({ id, title, thumbnail: { extension, path } }) => {
-
             if (path.lastIndexOf(IMG_NOT_AVALILABLE) === -1) {
                 const imgSrc = path + '/' + IMG_STANDART_XLARGE + '.' + extension;
+                const url = API_URL + URL_COMICS + '/' + id + '/' + URL_CHARACTERS;
+
                 htmlContent += `
-            <li class="comics__item">
-                <span class="comics__name">${title}</span>
-                <img class="comics__img" src="${imgSrc}" />
-            </li>
-            `
+                    <li class="comics__item" data-uri="${url}">
+                        <span class="comics__name">${title}</span>
+                        <img class="comics__img" src="${imgSrc}" />
+                    </li>
+                    `
             }
         })
 
@@ -30,6 +31,16 @@ class Comics {
         `
 
         ROOT_INDEX.innerHTML = htmlWrapper;
+    }
+
+    eventListener() {
+        document.querySelectorAll('.comics__item').forEach(element => {
+            const url = element.getAttribute('data-uri');
+
+            element.addEventListener('click', () => {
+                console.log(url);
+            })
+        })
     }
 }
 
